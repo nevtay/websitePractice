4ddb0e428a7b63534de8b3c738caeafacf253b02
nevtay <nevintay@gmail.com>
2019-04-12 17:54:23 +0800

    design + copy changes; to add scrolling onclick

---
+++ script.js
@@ -0,0 +1,12 @@
+const navAbout = document.querySelector('#nav-about');
+const navCompetencies = document.querySelector('#nav-competencies');
+const navContact = document.querySelector('#nav-contact');
+
+const about = document.querySelector('#about');
+const logo = document.querySelector('#logo');
+const skills = document.querySelector('#skills');
+
+navAbout.addEventListener('click', scroll());
+
+
+
