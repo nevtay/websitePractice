1f5288eaa23fe17a0c835905d549d3f1545c59d6
nevtay <nevintay@gmail.com>
2019-04-13 18:06:43 +0800

    scrolling animation added

--- script.js
+++ script.js
@@ -6,7 +6,46 @@ const about = document.querySelector('#about');
 const logo = document.querySelector('#logo');
 const skills = document.querySelector('#skills');
 
-navAbout.addEventListener('click', scroll());
+function smoothScroll(target, duration) {
+    var target = document.querySelector(target);
+    var targetPosition = target.getBoundingClientRect().top;
+    var startPosition = window.pageYOffset;
+    var distance = targetPosition - startPosition;
+    var startTime = null;
+
+    function animation(currentTime) {
+        if (startTime === null) {
+            startTime = currentTime;
+        }
+        var timeElapsed = currentTime - startTime;
+        var run = ease(timeElapsed, startPosition, distance, duration);
+        window.scrollTo(0, run);
+        if(timeElapsed < duration) {
+            requestAnimationFrame(animation);
+        }
+    }
+
+    function ease(t, b, c, d) {
+        t /= d/2;
+        if (t < 1) return c/2*t*t + b;
+        t--;
+        return -c/2 * (t*(t-2) - 1) + b;
+    }
+
+    requestAnimationFrame(animation);
+
+}
 
 
+navAbout.addEventListener('click', function() {
+    smoothScroll('#about', 1000);
+});
+
+navCompetencies.addEventListener('click', function() {
+    smoothScroll('#skills', 1000);
+});
+
+navContact.addEventListener('click', function() {
+    smoothScroll('.footer--contact', 1000);
+});
 
