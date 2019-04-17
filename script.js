da73190677956090b37821c4ecd0fb533ca6b15b
nevtay <nevintay@gmail.com>
2019-04-17 13:37:48 +0800

    Revert "adjusted UI"
    This reverts commit 24fa194a325a83167886653a7f0124d32506a20f.

--- script.js
+++ script.js
@@ -9,10 +9,10 @@ const skills = document.querySelector('#skills');
 function smoothScroll(target, duration) {
     event.preventDefault();
     var target = document.querySelector(target);
-    let targetPosition = target.getBoundingClientRect().top;
-    let startPosition = window.pageYOffset;
-    let distance = targetPosition - startPosition;
-    let startTime = null;
+    var targetPosition = target.getBoundingClientRect().top;
+    var startPosition = window.pageYOffset;
+    var distance = targetPosition - startPosition;
+    var startTime = null;
 
     function animation(currentTime) {
         if (startTime === null) {
@@ -21,16 +21,16 @@ function smoothScroll(target, duration) {
         var timeElapsed = currentTime - startTime;
         var run = ease(timeElapsed, startPosition, distance, duration);
         window.scrollTo(0, run);
-        if (timeElapsed < duration) {
+        if(timeElapsed < duration) {
             requestAnimationFrame(animation);
         }
     }
 
     function ease(t, b, c, d) {
-        t /= d / 2;
-        if (t < 1) return c / 2 * t * t + b;
+        t /= d/2;
+        if (t < 1) return c/2*t*t + b;
         t--;
-        return -c / 2 * (t * (t - 2) - 1) + b;
+        return -c/2 * (t*(t-2) - 1) + b;
     }
 
     requestAnimationFrame(animation);
@@ -38,15 +38,15 @@ function smoothScroll(target, duration) {
 }
 
 
-navAbout.addEventListener('click', function () {
-    smoothScroll('#about', 500);
+navAbout.addEventListener('click', function() {
+    smoothScroll('#about', 750);
 });
 
-navCompetencies.addEventListener('click', function () {
-    smoothScroll('#skills', 500);
+navCompetencies.addEventListener('click', function() {
+    smoothScroll('#skills', 750);
 });
 
-navContact.addEventListener('click', function () {
-    smoothScroll('.footer--contact', 500);
+navContact.addEventListener('click', function() {
+    smoothScroll('.footer--contact', 750);
 });
 
