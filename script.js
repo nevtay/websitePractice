682278b143d633db4ce79395001ead875aaa8c48
nevtay <nevintay@gmail.com>
2019-04-21 13:48:27 +0800

    test

---
+++ script.js
@@ -0,0 +1,52 @@
+const navAbout = document.querySelector('#nav-about');
+const navCompetencies = document.querySelector('#nav-competencies');
+const navContact = document.querySelector('#nav-contact');
+
+const about = document.querySelector('#about');
+const logo = document.querySelector('#logo');
+const skills = document.querySelector('#skills');
+
+function smoothScroll(target, duration) {
+    event.preventDefault();
+    var target = document.querySelector(target);
+    var targetPosition = target.getBoundingClientRect().top;
+    var startPosition = window.pageYOffset;
+    var distance = targetPosition - startPosition;
+    var startTime = null;
+
+    function animation(currentTime) {
+        if (startTime === null) {
+            startTime = currentTime;
+        }
+        var timeElapsed = currentTime - startTime;
+        var run = ease(timeElapsed, startPosition, distance, duration);
+        window.scrollTo(0, run);
+        if(timeElapsed < duration) {
+            requestAnimationFrame(animation);
+        }
+    }
+
+    function ease(t, b, c, d) {
+        t /= d/2;
+        if (t < 1) return c/2*t*t + b;
+        t--;
+        return -c/2 * (t*(t-2) - 1) + b;
+    }
+
+    requestAnimationFrame(animation);
+
+}
+
+
+navAbout.addEventListener('click', function() {
+    smoothScroll('#about', 750);
+});
+
+navCompetencies.addEventListener('click', function() {
+    smoothScroll('#skills', 750);
+});
+
+navContact.addEventListener('click', function() {
+    smoothScroll('.footer--contact', 750);
+});
+
